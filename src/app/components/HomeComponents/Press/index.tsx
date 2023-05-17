7c377266167f42bb98cec2f872d6122b64f4f7d5
deniyaldanidan <deniyaldanidan@gmail.com>
2023-05-17 12:52:19 +0000

    some improvements currently working on press-section in homepage

--- src/app/components/HomeComponents/Press/index.tsx
+++ src/app/components/HomeComponents/Press/index.tsx
@@ -1,10 +1,45 @@
 'use client';
 
+import Image from 'next/image';
+import { pressList } from '../data';
+import styles from './index.module.scss';
+import Link from 'next/link';
+import useEmblaCarousel from 'embla-carousel-react';
 
-export default function Press(){
+
+
+export default function Press() {
+    const [emblaRef] = useEmblaCarousel({ slidesToScroll: 2, startIndex: 0, align: "start", dragFree: true, loop: true });
+
     return (
-        <div>
-            Press Section Comes here
+        <div className={styles.press_section}>
+            <div className={styles.title}>
+                Press
+            </div>
+            <div className={styles.embla}>
+                <div className={styles.embla__viewport} ref={emblaRef}>
+                    <div className={styles.embla__container}>
+                        {
+                            pressList.map(press => (
+                                <div className={styles.embla__slide} key={press.title}>
+                                    <Image src={press.image} alt={press.title} />
+                                    <div className={styles.slide_contents}>
+                                        <div className={styles.slide__title}>{press.title}</div>
+                                        <div className={styles.slide__meta}>
+                                            <span>{press.by}</span>
+                                            <span>{press.date}</span>
+                                        </div>
+                                        <Link href='https://danithedev.tech'>Read More</Link>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+                <div className={styles.btns}>
+
+                </div>
+            </div>
         </div>
     )
 }
--- src/app/components/HomeComponents/data.tsx
+++ src/app/components/HomeComponents/data.tsx
@@ -13,6 +13,17 @@ import frenchRoast from '../../assets/coffee-shop/french-roast.jpg';
 import malabarRoast from '../../assets/coffee-shop/malabar-estate.jpg';
 import nyNights from '../../assets/coffee-shop/new-york-nights.jpg';
 
+import blog1 from '../../assets/blog/blog1.jpg';
+import blog2 from '../../assets/blog/blog2.jpg';
+import blog3 from '../../assets/blog/blog3.jpg';
+import blog4 from '../../assets/blog/blog4.jpg';
+import blog5 from '../../assets/blog/blog5.jpg';
+import blog6 from '../../assets/blog/blog6.jpg';
+import blog7 from '../../assets/blog/blog7.jpg';
+import blog8 from '../../assets/blog/blog8.jpg';
+import blog9 from '../../assets/blog/blog9.jpg';
+
+
 export const categoryContent = [
     {
         image: beans,
@@ -107,3 +118,60 @@ export const bestsellers = [
         price: 500
     }*/
 ];
+
+export const pressList = [
+    {
+        title: "Local coffee brand brews up success",
+        image: blog1,
+        by: "Chennai Express",
+        date: "12, November, 2020"
+    },
+    {
+        title: "Spicy-Brew Goes Green with Sustainable Practices",
+        image: blog2,
+        by: "Trivandrum Times",
+        date: "5, December, 2022"
+    },
+    {
+        title: "Spicy-Brew Introduces Innovative New Flavors",
+        image: blog3,
+        by: "Kochi Express",
+        date: "21, Febraury, 2021"
+    },
+    {
+        title: "Local Coffee Brand Spicy-Brew Wins Award for Exceptional Quality",
+        image: blog4,
+        by: "Bombay Chronicles",
+        date: "10, July, 2019"
+    },
+    {
+        title: "Spicy-Brew Introduces Revolutionary Brewing Technology",
+        image: blog5,
+        by: "The Delhi Times",
+        date: "11, August, 2020"
+    },
+    {
+        title: "Famous Coffee Brand Spicy-Brew Offers Exclusive Behind-the-Scenes Tours",
+        image: blog6,
+        by: "Chennai Express",
+        date: "22, December, 2022"
+    },
+    {
+        title: "Famous Coffee Brand Offers Educational Workshops for Coffee Lovers",
+        image: blog7,
+        date: "29, December, 2022",
+        by: "The Daily News"
+    },
+    {
+        title: "Spicy-Brew Sets New Record for Sales Growth",
+        image: blog8,
+        by: "Chennai Express",
+        date: "25, April, 2023",
+    },
+    {
+        title: "Local Coffee Brand Goes the Extra Mile for Environmental Sustainability",
+        image: blog9,
+        by: "Trivandrum Times",
+        date: "20, November, 2022"
+    }
+]
