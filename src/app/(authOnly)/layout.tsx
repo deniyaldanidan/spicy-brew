a9383ef6c266a677319f150a8a1fc40208f7a94e
deniyaldanidan <deniyaldanidan@gmail.com>
2023-08-20 23:49:05 +0530

    trials-6

--- src/app/(authOnly)/layout.tsx
+++ src/app/(authOnly)/layout.tsx
@@ -1,17 +1,19 @@
-"use client";
+// "use client";
 
 import React from "react";
 import AuthOnly from "@/app/_components/AuthOnly";
 import { Metadata } from "next";
-import dynamic from "next/dynamic";
-import Loader1 from "../_components/loaders/Loader1";
+// import dynamic from "next/dynamic";
+// import Loader1 from "../_components/loaders/Loader1";
 
 // const AuthOnly = dynamic(()=>import("@/app/_components/AuthOnly"), {ssr: false, loading: ()=><Loader1 />})
 
-export const metadata:Metadata = {
+export const metadata: Metadata = {
     title: "DashBoard"
 }
 
-export default function Layout({children}:{children:React.ReactNode}){
-    return <AuthOnly>{children}</AuthOnly>
+export default function Layout({ authroutes }: { authroutes: React.ReactNode}) {
+    return (
+        <AuthOnly>{authroutes}</AuthOnly>
+    )
 }
